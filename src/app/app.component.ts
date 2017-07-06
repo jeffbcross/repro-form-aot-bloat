dee0fdd0b9e377a3d3119b388fe116fcce78a505
Jeff Cross <middlefloor@gmail.com>
2017-07-05 17:21:10 -0700

    add forms to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+   selector: 'app-root',
+   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app works!';
+  jeffsForm = new FormGroup({
+    control1: new FormControl('control1', [Validators.required]),
+    control2: new FormControl('control2', [Validators.required]),
+    control3: new FormControl('control3', [Validators.required])
+  });
 }
